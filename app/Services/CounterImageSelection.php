254360398c2b2a9595d07b33f84732666f30be89
denindra <denin.resky@gmail.com>
2022-07-28 13:53:16 +0700

    initiate

--- app/Services/CounterImageSelection.php
+++ app/Services/CounterImageSelection.php
@@ -10,29 +10,84 @@ class CounterImageSelection
 {
     public function imageSelection($totalCount) {
         
-        if($totalCount >= 0 && $totalCount <= 10) {
+        if($totalCount >= 0 && $totalCount <= 3) {
 
-            $shareImage = '0-10 share';
-            $coverImage = '0-10 cover';
+            $coverImage = env('APP_URL').'/images/artwork/Artwork01.png';
         }
-       else  if($totalCount >= 11 && $totalCount <= 20) {
+       else  if($totalCount >= 4 && $totalCount <= 6) {
 
-            $shareImage = '11-20 share';
-            $coverImage = '11-20 cover';
+            $coverImage = env('APP_URL').'/images/artwork/Artwork02.png';
         }
-       else  if($totalCount >= 21 && $totalCount <= 30) {
+       else  if($totalCount >= 7 && $totalCount <= 9) {
 
-            $shareImage = '21-30 share';
-            $coverImage = '21-30 cover';
+            $coverImage = env('APP_URL').'/images/artwork/Artwork03.png';
         }
-        else if($totalCount >= 31 ) {
+        else  if($totalCount >= 10 && $totalCount <= 12) {
 
-            $shareImage = '31-40 share';
-            $coverImage = '31-40 cover';
+            $coverImage = env('APP_URL').'/images/artwork/Artwork04.png';
         }
-        else  
-        {
-            $shareImage = null;
+        else  if($totalCount >= 13 && $totalCount <= 15) {
+
+            $coverImage = env('APP_URL').'/images/artwork/Artwork05.png';
+        }
+        else  if($totalCount >= 16 && $totalCount <= 18) {
+
+            $coverImage = env('APP_URL').'/images/artwork/Artwork06.png';
+        }
+        else  if($totalCount >= 19 && $totalCount <= 21) {
+
+            $coverImage = env('APP_URL').'/images/artwork/Artwork07.png';
+        }
+        else  if($totalCount >= 22 && $totalCount <= 24) {
+
+            $coverImage = env('APP_URL').'/images/artwork/Artwork08.png';
+        }
+        else  if($totalCount >= 25 && $totalCount <= 27) {
+
+            $coverImage = env('APP_URL').'/images/artwork/Artwork09.png';
+        }
+        else  if($totalCount >= 28 && $totalCount <= 30) {
+
+            $coverImage = env('APP_URL').'/images/artwork/Artwork10.png';
+        }
+        else  if($totalCount >= 31 && $totalCount <= 33) {
+
+            $coverImage = env('APP_URL').'/images/artwork/Artwork11.png';
+        }
+        else  if($totalCount >= 34 && $totalCount <= 36) {
+
+            $coverImage = env('APP_URL').'/images/artwork/Artwork12.png';
+        }
+        else  if($totalCount >= 37 && $totalCount <= 39) {
+
+            $coverImage = env('APP_URL').'/images/artwork/Artwork13.png';
+        }
+        else  if($totalCount >= 40 && $totalCount <= 42) {
+
+            $coverImage = env('APP_URL').'/images/artwork/Artwork14.png';
+        }
+        else  if($totalCount >= 43 && $totalCount <= 45) {
+
+            $coverImage = env('APP_URL').'/images/artwork/Artwork15.png';
+        }
+        else  if($totalCount >= 46 && $totalCount <= 48) {
+
+            $coverImage = env('APP_URL').'/images/artwork/Artwork16.png';
+        }
+        else  if($totalCount >= 49 && $totalCount <= 51) {
+
+            $coverImage = env('APP_URL').'/images/artwork/Artwork17.png';
+        }
+        else  if($totalCount >= 52 && $totalCount <= 54) {
+
+            $coverImage = env('APP_URL').'/images/artwork/Artwork18.png';
+        }
+        else if($totalCount >= 55 ) {
+
+            $coverImage = env('APP_URL').'/images/artwork/Artwork18.png';
+        }
+        else   {
+            
             $coverImage = null;
         }
        
@@ -41,7 +96,6 @@ class CounterImageSelection
             'status' => true,
             'response'=>[
                 'coverImage' =>$coverImage,
-                'shareImage' =>$shareImage,
                 'getCounter' =>$totalCount
             ],
             'message'=>'get image progress success'
